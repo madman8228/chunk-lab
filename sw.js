ebf2fc7f45a2114daccfc040339790c942265b9e
madman8228 <madman8228@gmail.com>
2026-08-31 17:54:15 +0800

    fix: replace native deck import alerts

--- sw.js
+++ sw.js
@@ -18,7 +18,7 @@
  */
 /* eslint-disable */
 /* 每次前端资源变更都递增版本，避免旧版 HTML/CSS 被长期命中。 */
-const CACHE = 'chunklab-v29';
+const CACHE = 'chunklab-v30';
 const PRECACHE = [
   '/main.html',
   '/manifest.json',
